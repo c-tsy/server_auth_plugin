1f3e8335dc8c5cc7fa98d404ff91a9f29b526d48
CastleMY <490523604@qq.com>
2019-06-07 13:42:41 +0800

    获取指定用户的团队信息

--- src/lib/controller/User.ts
+++ src/lib/controller/User.ts
@@ -15,36 +15,39 @@ export default class User extends BController {
      * 获取我的团队信息
      * @param param0 
      */
-    async myteam({ P, N, Type }) {
-        let uid = await this._session('UID');
-        if (!uid) {
-            throw new Error('未登录')
-        }
-        P = P || 1;
-        N = N || 10;
-        let level = await this.M(Models.Levels).where({ PUID: uid }).order('Level ASC').getFields('Level');
-        if (undefined === level) {
-            return {
-                L: [],
-                T: 0,
-                P: P,
-                N: N,
-            }
-        }
-        let r = await this.M(Models.Levels).where({ PUID: uid, Level: level }).fields('UID').page(P, N).selectAndCount();
-        return {
-            L: await this.R(Models.Users).where({ UID: { in: array_columns(r.rows, 'UID') } }).select(),
-            T: r.count,
-            P: P,
-            N: N,
-        }
-    }
+    // async myteam({ P, N, Type }) {
+    //     let uid = await this._session('UID');
+    //     if (!uid) {
+    //         throw new Error('未登录')
+    //     }
+    //     P = P || 1;
+    //     N = N || 10;
+    //     let level = await this.M(Models.Levels).where({ PUID: uid }).order('Level ASC').getFields('Level');
+    //     if (undefined === level) {
+    //         return {
+    //             L: [],
+    //             T: 0,
+    //             P: P,
+    //             N: N,
+    //         }
+    //     }
+    //     let r = await this.M(Models.Levels).where({ PUID: uid, Level: level }).fields('UID').page(P, N).selectAndCount();
+    //     return {
+    //         L: await this.R(Models.Users).where({ UID: { in: array_columns(r.rows, 'UID') } }).select(),
+    //         T: r.count,
+    //         P: P,
+    //         N: N,
+    //     }
+    // }
     /**
      * 获取指定成远的团队信息
      * @param param0 
      */
-    async team({ UID, P, N }) {
+    async myteam({ UID, P, N }) {
         if (!UID) { UID = await this._session('UID'); }
+        if (!UID) {
+            throw new Error('未指定用户')
+        }
         let mine = await this.M(Models.Users).where({ UID }).getFields('TNum');
         return {
             L: await this.R(Models.Users).where({ PUID: UID }).page(P || 1, N || 10).select(),
