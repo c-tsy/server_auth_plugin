3d27629e205be8c331374f176a54c654ac8fa096
castleiMac <castle@tansuyun.cn>
2019-08-12 22:02:02 +0800

    添加Rule接口

--- src/lib/controller/Rule.ts
+++ src/lib/controller/Rule.ts
@@ -1,19 +1,6 @@
 import { Models } from "../iface/models"
 import auth from '../..'
-import { BController } from '../lib/controller';
-export default class Rule extends BController{
-    
-    /**
-     * 查询权限组
-     */
-    async queryRule(data: {RID: string}) {
-        return await this.M(Models.Rule).where({RID: data.RID}).find()
-    }
+import { BController, CController } from '../lib/controller';
+export default class Rule extends CController {
 
-    /**
-     * 查询所有权限组
-     */
-    async queryRules() {
-        return await this.M(Models.Rule).select()
-    }
 }
