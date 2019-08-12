10bdda3a8630cad1548f1f17201bc53bfdc1409f
castleiMac <castle@tansuyun.cn>
2019-08-12 22:02:34 +0800

    添加用户操作接口

---
+++ src/lib/controller/Users.ts
@@ -0,0 +1,18 @@
+import Controller from '@ctsy/controller/dist/controller';
+export default class Users extends Controller {
+    // get _KeywordTable()
+    get _KeywordFields() {
+        return 'Name,Nick'.split(',')
+    }
+    get _KeywordTable() {
+        return 'Users'
+    }
+
+    async add() { }
+    async adds() { }
+    async save() { }
+    async saveW() { }
+    async del() { }
+    async delW() { }
+    async replaceW() { }
+}
